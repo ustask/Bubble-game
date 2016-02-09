9f7d4fb9c63703b0349ffc1ad4407ecf73074be3
ustask <kathleenustas@Kathleens-MacBook-Pro.local>
2016-02-09 18:23:34 -0500

    game update

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800,700);
+}
+
+function draw() {
+  background(248,252,193);
+  //pink vivacious
+  fill(201,12,57);
+  ellipse(80,80,100,100);
+  //blue lagoon
+  fill(22,147,167);
+  ellipse(250,170,200,200);
+  //green
+  fill(200,207,2);
+  ellipse(390,390,65,65);
+  //orange
+  fill(230,120,20);
+  ellipse(400,500,120,120);
+}
